66fe764cfdc777311996f419a232fa9e5228f287
aslanator <aslanator@mail.ru>
2019-11-13 23:26:09 +0300

    Now imageTile have size of image

--- code/web/src/ui/image/Tile.js
+++ code/web/src/ui/image/Tile.js
@@ -3,8 +3,51 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 // Component
-const Tile = (props) => {
-  const { children, image, width, height, style, shadow, ...others } = props
+class Tile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: 'none'
+    };
+  }
+
+  componentDidMount() {
+    const image = new Image();
+    image.onload = () => {
+      console.log(image);
+      this.setState({
+        width: image.width,
+        height: image.height,
+        display: "block"
+      });
+    };
+    image.src = this.props.src;
+  }
+
+  render() {
+    const { children, src, style, shadow, ...others } = this.props;
+    let { display, height, width } = this.state;
+    return (
+      <div style={Object.assign({ height, width, display }, style)} {...others}>
+        {children}
+
+        {/* language=CSS */}
+        <style jsx>{`
+        div {
+          background-image:url('${src}');
+          background-size: 100% auto;
+          box-shadow: ${shadow ? shadow : 'none'};
+        }
+      `}</style>
+      </div>
+    );
+  }
+}
+
+const Tile1 = (props) => {
+
+  // this will re render the view with new data
 
   return (
     <div style={Object.assign({ height, width }, style)} {...others}>
@@ -13,9 +56,9 @@ const Tile = (props) => {
       {/* language=CSS */}
       <style jsx>{`
         div {
-          background-image:url('${ image }');
+          background-image:url('${src}');
           background-size: 100% auto;
-          box-shadow: ${ shadow ? shadow : 'none' };
+          box-shadow: ${shadow ? shadow : 'none'};
         }
       `}</style>
     </div>
@@ -24,16 +67,16 @@ const Tile = (props) => {
 
 // Component Properties
 Tile.propTypes = {
-  image: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
   style: PropTypes.object,
   width: PropTypes.number,
   height: PropTypes.number,
   shadow: PropTypes.string
-}
+};
 Tile.defaultProps = {
   style: {},
   width: '100%',
   height: '100%'
-}
+};
 
 export default Tile
